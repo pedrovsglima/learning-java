a12a8afa8b35499df75f4b32d3a18e2fe074902e
Pedro Lima <p.vitor31@hotmail.com>
2022-07-10 12:06:55 -0300

    utilizando getters e setters

--- 2-product-data/src/App.java
+++ 2-product-data/src/App.java
@@ -5,25 +5,36 @@ public class App {
         
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter product data: ");
+        // 1
+        Product prod = new Product();
+        prod.setName("TV");
+        prod.setPrice(500);;
+        prod.addProducts(50);
+        System.out.println(prod);
 
+        // 2
+        System.out.println("Enter product data: ");
         System.out.print("Name: ");
         String name = sc.nextLine();
-
         System.out.print("Price: ");
         double price = sc.nextDouble();
 
+        Product prod1 = new Product(name, price);
+        System.out.println(prod1);
+
+        // 3
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name1 = sc.nextLine();
+        System.out.print("Price: ");
+        double price1 = sc.nextDouble();
         System.out.print("Quantity in stock: ");
         int quantity = sc.nextInt();
 
-        Product prod = new Product(name, price, quantity);
-        prod.removeProducts(50);
-        System.out.println(prod);
+        Product prod2 = new Product(name1, price1, quantity);
+        System.out.println(prod2);
 
-        Product prod1 = new Product(name, price);
-        prod1.addProducts(50);
-        System.out.println(prod1);
-
+        
         sc.close();
     }
 }
--- 2-product-data/src/Product.java
+++ 2-product-data/src/Product.java
@@ -1,8 +1,8 @@
 public class Product {
     
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public Product() {
     }
@@ -16,6 +16,26 @@ public class Product {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
     public double totalValueInStock() {
